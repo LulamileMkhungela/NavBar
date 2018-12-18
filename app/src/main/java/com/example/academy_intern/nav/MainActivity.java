440eb8a411c21944caf8dd5ddaceeb58b29dfd2c
Lulamile Mkhungela <lulamile@thedigitalacademy.co.za>
2018-12-18 14:50:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/academy_intern/nav/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.academy_intern.nav;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etSearch;
+    Button btnSearch,btnProfile;
+    ViewPager mViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+
+    }
+}
